31d8ea97c5fadb3534abb421e89ddc483fe048f4
deepak.siddhi <deepak.siddhi@timesinternet.in>
2025-04-23 14:32:34 +0530

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+
+
+
+const app = express();
+
+const port = process.env.PORT || 8000
+
+app.listen(port,(req,res)=>{
+    console.log(`Port is listening on port:${port}`);
+    
+})
+
+
+
+
+
+
+
+
+
